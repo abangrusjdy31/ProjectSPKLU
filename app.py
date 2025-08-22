379f019e86c7a7c4abb5f8203e401e2e9eb89d69
abangrusjdy31 <134632111+abangrusjdy31@users.noreply.github.com>
2025-08-22 09:29:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def get_base64_image(image_path):
     return encoded
 
 # Logo
-logo_base64 = get_base64_image("/content/logo spklu.png")
+logo_base64 = get_base64_image("logo spklu.png")
 
 # ==== Sidebar ====
 with st.sidebar:
